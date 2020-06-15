1b9014ef81dba5daa7722ae1248882ca0bfdbc48
kmurata798 <kmurata798@gmail.com>
2020-06-14 22:41:41 -0700

    [Add] Setup the views folder for handlebars templates.

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const app = express()
-const port = 3000
+// const methodOverride = require('method-override')
+// const bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+
+
+// // The following line must appear AFTER const app = express() and before your routes!
+// app.use(bodyParser.urlencoded({ extended: true }));
+// // Use the MethodOverride I imported to change POST requests to PUT requests
+// app.use(methodOverride('_method'))
+// Use "main" as our default layout
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+// Use handlebars to render
+app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-    return res.render('main');
+    return res.render('home');
     })
+
+const port = 3000
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
