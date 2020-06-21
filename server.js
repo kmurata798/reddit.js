a1f40878d371850609764c0d339b0906dfa36801
kmurata798 <kmurata798@gmail.com>
2020-06-21 00:28:37 -0700

    [Add] Figuring out how to allow users to submit/save posts

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
+// Initialize Express
 const express = require('express')
 const app = express()
+
+// Middleware
 // const methodOverride = require('method-override')
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 var exphbs = require('express-handlebars');
 
+// Set db
+const database = require('./data/reddit-db');
+const posts = require('./controllers/posts.js')(app);
 
+app.use(bodyParser.json());
 // // The following line must appear AFTER const app = express() and before your routes!
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
 // // Use the MethodOverride I imported to change POST requests to PUT requests
 // app.use(methodOverride('_method'))
 // Use "main" as our default layout
@@ -14,9 +23,14 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 // Use handlebars to render
 app.set('view engine', 'handlebars');
 
+
 app.get('/', (req, res) => {
     return res.render('home');
     })
+
+app.get('/posts/new', (req, res) => {
+    res.render('posts-new');
+})
 
 const port = 3000
 
